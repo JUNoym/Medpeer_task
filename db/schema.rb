4db244b33e33faafc5e54e849855bcb9f25a9928
JUNoym <junnyaooyama@gmail.com>
2022-01-26 19:45:00 +0900

    categoriesテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_26_100410) do
+ActiveRecord::Schema.define(version: 2022_01_26_103329) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "tests", force: :cascade do |t|
     t.string "title"
