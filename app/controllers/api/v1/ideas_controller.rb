10afa25f2f4bddc81044edc62a2ff439b2bbf92e
JUNoym <junnyaooyama@gmail.com>
2022-01-28 20:23:32 +0900

    アイデアコントローラーとモデルを作成

---
+++ app/controllers/api/v1/ideas_controller.rb
@@ -0,0 +1,18 @@
+class Api::V1::IdeasController < ApplicationController
+
+    def show
+        ideas = Idea.response_ideas(params[:category_name])
+        render json: { data: ideas }
+      rescue StandardError
+        render json: { status: 404 }
+    end
+  
+    def create
+        category = Category.find_or_create_by!(name: params[:category_name])
+        category.ideas.create!(body: params[:body])
+        render json: { status: 201 }
+      rescue StandardError
+        render json: { status: 422 }
+    end
+    
+end
--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,3 +1,24 @@
 class Idea < ApplicationRecord
   belongs_to :category
+  validates :body, presence: true
+
+  def self.response_ideas(category_name)
+    if category_name.present?
+      category = Category.find_by!(name: category_name)
+      ideas = category.ideas
+    else
+      ideas = Idea.all
+    end
+    Idea.format_idea(ideas)
+  end
+
+  def self.format_idea(ideas)
+    ideas.map do |idea|
+      {
+        id: idea.id,
+        category: idea.category.name,
+        body: idea.body
+      }
+    end
+  end
 end
