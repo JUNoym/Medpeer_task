55494fe3f2fe6edec376e633548c2dd051148ce5
JUNoym <junnyaooyama@gmail.com>
2022-01-29 20:14:15 +0900

    名前を登録することができているかを確認するテストを書く

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_26_105801) do
+ActiveRecord::Schema.define(version: 2022_01_29_102855) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name", null: false
@@ -28,6 +28,13 @@ ActiveRecord::Schema.define(version: 2022_01_26_105801) do
 
   create_table "tests", force: :cascade do |t|
     t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
