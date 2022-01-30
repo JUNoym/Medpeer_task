526d9c933f5f83af12a27348c3c05ce48846cebc
JUNoym <junnyaooyama@gmail.com>
2022-01-30 20:30:45 +0900

    アイデアを作成できるかをテスト

--- spec/requests/api/v1/ideas_spec.rb
+++ spec/requests/api/v1/ideas_spec.rb
@@ -1,15 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Idea", type: :request do
-  describe 'GET request' do
-    describe 'success' do
-      it 'can get all idea' do
+    describe 'GET request' do
+        describe 'success' do
+        it 'can get all idea' do
 
-        get '/api/v1/ideas'
-        json = JSON.parse(response.body)
+            get '/api/v1/ideas'
+            json = JSON.parse(response.body)
 
-        expect(response.status).to eq(200)
-      end
+            expect(response.status).to eq(200)
+        end
+        end
     end
-end
+
+    describe 'POST request' do
+        describe 'success' do
+        it 'can create idea' do
+
+            expect {
+                post '/api/v1/ideas', params: { category_name: 'test-category', body: "test-idea" }
+            }.to change(Idea, :count).by(+1)
+            json = JSON.parse(response.body)
+
+            expect(response.status).to eq(200)
+            expect(json['status']).to eq(201)
+        end
+        end
+    end
 end
