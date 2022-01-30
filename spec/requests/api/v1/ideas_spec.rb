38dfc6689403d82509e783b35b71da6a801f3a9a
JUNoym <junnyaooyama@gmail.com>
2022-01-30 20:49:42 +0900

    テストケース追加

--- spec/requests/api/v1/ideas_spec.rb
+++ spec/requests/api/v1/ideas_spec.rb
@@ -15,14 +15,30 @@ RSpec.describe "Api::V1::Idea", type: :request do
 
     describe 'POST request' do
         describe 'success' do
-            it 'can create idea' do
+            it 'can create idea with new category' do
                 expect {
                     post '/api/v1/ideas', params: { category_name: 'test-category', body: "test-idea" }
-                }.to change(Idea, :count).by(+1)
+                }.to change(Idea, :count).by(+1).and change(Category, :count).by(+1)
                 json = JSON.parse(response.body)
 
                 expect(response.status).to eq(200)
                 expect(json['status']).to eq(201)
+            end
+        end
+        describe 'failed' do
+            it 'no category' do
+                post '/api/v1/ideas', params: { category_name: '', body: "test-idea" }
+                json = JSON.parse(response.body)
+
+                expect(response.status).to eq(200)
+                expect(json['status']).to eq(422)
+            end
+            it 'no body' do
+                post '/api/v1/ideas', params: { category_name: 'test-category', body: '' }
+                json = JSON.parse(response.body)
+
+                expect(response.status).to eq(200)
+                expect(json['status']).to eq(422)
             end
         end
     end
